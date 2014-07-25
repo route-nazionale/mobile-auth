7e27728ce4809119b0b00f8fdb6da6ef6b7c1cb3
Luca Lancioni <cuginolancio@gmail.com>
2014-07-25 03:19:22 +0200

    aggiunti parametri di default

---
+++ config/params.dist.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * User: lancio
+ * Date: 25/07/14
+ * Time: 01:56
+ */
+
+define('APP_DEBUG', false);
+
+define('HTTPS_REQUIRED', true);
+
+define('API_AUTH', "https://localhost/auth");
+
+/*
+ * se non sono presenti le cerca su db
+ * iv e key di decrypt
+ */
+define('AES_IV', false);//base64_decode('aE5RaTRTTmJTNnFuSXFRQQ==')); //iv 16byte
+define('AES_KEY', false);//base64_decode('TVkwNVVreVpJak5yTWtVZUYwcTRxeXE5RllCcXZuM0U=')); //key 32byte
+
+/**
+ * parametri accesso db
+ */
+define('MYSQL_HOST', 'localhost');
+define('MYSQL_PORT', '3306');
+
+/**
+ * AES accesso al db per prelevare iv e key di decrypt
+ */
+define('MYSQL_DB_AES', 'db');
+define('MYSQL_USER_AES', 'user');
+define('MYSQL_PASS_AES', 'password');
+
+/**
+ * AES accesso al db per verificare il capospalla
+ */
+define('MYSQL_DB_VARCHI', 'db');
+define('MYSQL_USER_VARCHI', 'user');
+define('MYSQL_PASS_VARCHI', 'password');
+
+/**
+ * nome del db sqlite
+ */
+define('SQLITE_DB_FILENAME', 'database.db');
