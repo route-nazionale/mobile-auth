861b87f5753bc0636fd6270fab8a2cf8973cfdb6
Luca Lancioni <cuginolancio@gmail.com>
2014-07-31 01:21:15 +0200

    fixato l'autenticazione fake

--- src/Rn2014/Auth/AuthFake.php
+++ src/Rn2014/Auth/AuthFake.php
@@ -45,7 +45,16 @@ class AuthFake implements AuthInterface
             'user_agent' => $request->headers->get('User-Agent'),
         ];
 
-        if (isset($this->users[$cu]) && ("security.full" == $this->users[$cu][1] || "security.meal" == $this->users[$cu][1] || $this->secondaryAuth == $this->users[$cu][1])) {
+        if (isset($this->users[$cu]) && $group == $this->users[$cu][1]) {
+            $result = [
+                "code" => 200,
+                "result" => "security",
+            ];
+            $context['result'] = $result;
+            $this->logger->addInfo("FAKE OK {$this->users[$cu][1]}", $context);
+            return $result;
+
+        } elseif (isset($this->users[$cu]) && $this->secondaryAuth == $this->users[$cu][1]) {
             $result = [
                 "code" => 200,
                 "result" => $this->users[$cu][1],
